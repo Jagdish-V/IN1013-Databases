1270cef79df6de3104fac68ff4f057ca8437d9ea
Jagdish Vaghela <jagdish.vaghela@city.ac.uk>
2024-10-30 15:20:57 +0000

    Added question number as commets

--- HW 5/task2.sql
+++ HW 5/task2.sql
@@ -1,5 +1,14 @@
+-- 1.
 SELECT SUM(bill_total) AS 'Income' FROM restbill;
+
+-- 2.
 SELECT SUM(bill_total) AS 'Feb Income' FROM restbill  WHERE bill_date  LIKE '1602%';
+
+-- 3.
 SELECT AVG(bill_total) AS 'Average bill amount' FROM restbill; 
+
+-- 4.
 SELECT MIN(no_of_seats) AS 'MAX', MIN(no_of_seats) AS 'Max', AVG(no_of_seats) AS 'Avg' FROM restrest_table;
+
+-- 5.
 SELECT DISTINCT COUNT(headwaiter) AS 'Table Served' FROM reststaff WHERE staff_no = 2 OR staff_no = 4;
--- HW 5/task3.sql
+++ HW 5/task3.sql
@@ -1,6 +1,17 @@
+-- 1.
 SELECT first_name, surname FROM restStaff INNER JOIN restBill ON restStaff.staff_no = restBill.waiter_no WHERE cust_name = 'Tanya Singh';
+
+-- 2.
 SELECT room_date FROM restRoom_management INNER JOIN restStaff ON restRoom_management.headwaiter = restStaff.staff_no  WHERE room_name = 'Green' AND first_name = 'Charles' AND room_date LIKE '1602%';
+
+-- 3.
 SELECT T2.first_name, T2.surname FROM restStaff AS T1 INNER JOIN restStaff AS T2 ON T1.headwaiter = T2.headwaiter WHERE T1.first_name = 'Zoe' AND T1.staff_no != T2.staff_no;
+
+-- 4.
 SELECT cust_name, bill_total, first_name FROM restBill INNER JOIN restStaff ON restBill.waiter_no = restStaff.staff_no ORDER BY bill_total DESC;
+
+-- 5.
 SELECT first_name, surname FROM restStaff AS T1 INNER JOIN restBill AS T2 ON INNER JOIN restStaff.staff_no = restBill.waiter_no WHERE bill_no = 14 AND bill_no = 17;
+
+-- 6.
 SELECT first_name, surname FROM restStaff INNER JOIN restRoom_management ON restRoom_management.headwaiter = restStaff.staff_no WHERE room_name = 'Blue' AND room_date = '160312';
